a55cfc4b4380e81c3e24e8b22357600bf3fd42aa
Abdulloh <abdullohbahromjonv@gmail.com>
2023-12-16 01:05:52 +0500

    Initial Commit

---
+++ Test_Project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Test_Project
+//
+//  Created by Abdulloh on 16/12/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Test_Project/Test_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_ProjectApp.swift
+//  Test_Project
+//
+//  Created by Abdulloh on 16/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
